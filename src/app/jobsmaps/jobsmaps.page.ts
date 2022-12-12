5fd21e28474358b34accd6f79961b33bb698acc2
moavizq5555 <57653982+Najam-khan@users.noreply.github.com>
2022-12-12 14:13:29 +0500

    commit by najam

--- src/app/jobsmaps/jobsmaps.page.ts
+++ src/app/jobsmaps/jobsmaps.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { GoogleMap } from '@capacitor/google-maps';
+import { GoogleMap, Marker } from '@capacitor/google-maps';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
 @Component({
@@ -8,8 +8,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./jobsmaps.page.scss'],
 })
 export class JobsmapsPage implements OnInit {
-  @ViewChild('map') mapRef: ElementRef | undefined;
-  map: GoogleMap | undefined;
+  @ViewChild('map') mapRef: any = ElementRef;
+  map: any = GoogleMap;
+  markerscheck = [
+    {
+      coordinate: {
+        lat: 30.1575,
+        lng: 71.5249,
+      }
+    },
+    {
+      coordinate: {
+        lat: 31.5204,
+        lng: 74.3587,
+      }
+    }
+  ]
+
   constructor(public router: Router) { }
 
   ngOnInit() {
@@ -20,6 +35,7 @@ export class JobsmapsPage implements OnInit {
   }
 
   async createMap() {
+    // AIzaSyAncWVozZi9mUrnaxdDJJE_rgRY5M-wD54
     this.map = await GoogleMap.create({
       id: 'my-map', // Unique identifier for this map instance
       element: this.mapRef?.nativeElement, // reference to the capacitor-google-map element
@@ -28,15 +44,56 @@ export class JobsmapsPage implements OnInit {
       config: {
         center: {
           // The initial position to be rendered by the map
-          lat: 33.6,
-          lng: -117.9,
+          lat: 30.1575,
+          lng: 71.5249,
         },
+
         zoom: 8, // The initial zoom level to be rendered by the map
       },
+
     });
+    // const markerId = await this.map.addMarker({
+    //   coordinate: {
+    //     lat: 30.1575,
+    //     lng: 71.5249,
+    //   }
+    // });
+    // const markerId1 = await this.map.addMarker({
+    //   coordinate: {
+    //     lat: 31.5204,
+    //     lng: 74.3587,
+    //   }
+    // });
+    this.addmarkers();
+
   }
 
+  async addmarkers() {
+    // const markers: Marker[] = [
+    //   {
+    //     coordinate: {
+    //       lat: 30.1575,
+    //       lng: 71.5249,
+    //     }
+    //   },
+    //   {
+    //     coordinate: {
+    //       lat: 31.5204,
+    //       lng: 74.3587,
+    //     }
+    //   }
 
+    // ];
+    await this.map.addMarkers(this.markerscheck);
+    this.map.setOnMarkerClickListener(async (marker: any) => {
+      console.log(marker);
+
+    })
+    this.map.setOnInfoWindowClickListener(async (marker: any) => {
+      console.log('info', marker);
+
+    })
+  }
   goToLogin() {
     this.router.navigate(['signin'])
   }
--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-
+// import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
+import { isPlatform } from '@ionic/angular';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.page.html',
@@ -11,10 +12,22 @@ export class SigninPage implements OnInit {
   constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
+    // if (!isPlatform('capacitor')) {
+    //   GoogleAuth.initialize();
+    // }
   }
 
   togglePass() {
     this.showPass = !this.showPass;
+  }
+
+
+  async doLogin() {
+    // const user = await GoogleAuth.signIn();
+    // if (user) {
+    //   console.log('user details::', user);
+
+    // }
   }
 
   forgot() {
